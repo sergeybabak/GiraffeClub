bae1748f20544e520e165d9ef2882828ece1b9c0
SergB <sergey.bab7@gmail.com>
2024-12-19 16:10:13 +0200

    promo end before media

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,16 @@
 const promo__btn = document.querySelector('.promo__btn'),
       message = document.querySelector('.message'),
       message__windows = document.querySelectorAll('.message__window'),
-      message__close = document.querySelectorAll('.message__close');
+      message__close = document.querySelectorAll('.message__close'),
+      form = document.querySelector('form');
 
 function visibleElement(i) {
+    displayElement();
+    // noneElement(i);
+    if (i==2) {
+        message__windows[0].classList.add('none');
+        message__windows[1].classList.add('none');
+    }
     message.classList.remove('hidden');
     message.classList.add('visible');
     message__windows[i].classList.remove('hidden');
@@ -14,6 +21,22 @@ function hiddenElement(i) {
     message__windows[i].classList.remove('visible');
     message.classList.add('hidden');
     message__windows[i].classList.add('hidden');
+    
+    // displayElement(i);
+}
+function noneElement(i) {
+    message__windows.forEach((mw, item) => {
+        if (item!=i) {
+            message__windows[item].classList.add('none');
+        } 
+    });
+}
+function displayElement() {
+    message__windows.forEach((mw, item) => {
+        message__windows.forEach((mw, item) => {
+            message__windows[item].classList.remove('none');
+        });
+    });
 }
 promo__btn.addEventListener('click', ()=> {
     visibleElement(0);
@@ -22,6 +45,12 @@ message__close.forEach((mc, i) => {
     mc.addEventListener('click', () => {
         hiddenElement(i);
     });
+});
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    hiddenElement(0);
+    
+    visibleElement(2);
 });
 
 
